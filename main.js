57a045f232b204fdea9a7f544e42304591f23549
ChristopherRoth01 <47116031+ChristopherRoth01@users.noreply.github.com>
2021-11-21 15:07:36 +0100

    gitignore + first files

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Main function. Builds first Scene with three.js
+ */
+function main() {
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera();
+}
+/*
+Execute main function as start.
+ */
+main();
